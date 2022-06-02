c1208f054eff6ce3e39318fe58554acdd83a7c6d
Touyama Hiroto <e205708@ie.u-ryukyu.ac.jp>
2022-06-02 15:56:39 +0900

    create models

--- kadai_kanri/kadai_kanri/Model/Assignment.swift
+++ kadai_kanri/kadai_kanri/Model/Assignment.swift
@@ -8,3 +8,21 @@
 import Foundation
 import RealmSwift
 
+class Assignment: Object, ObjectKeyIdentifiable{
+    @Persisted(primaryKey: true) var _id: ObjectId //ID
+    @Persisted var assignmentName: String //課題名
+    @Persisted var detail: String //課題詳細
+    @Persisted var limitDate: Date //期限
+    @Persisted var duration: Int //所要時間
+    @Persisted var className: String //科目名
+    
+    
+    convenience init(assigmentName: String, detail: String,limitDate: Date ,duration: Int,className: String){
+        self.init()
+        self.assignmentName = assigmentName
+        self.detail = detail
+        self.limitDate = limitDate
+        self.duration = duration
+        self.className = className
+    }
+}
---
+++ kadai_kanri/kadai_kanri/Model/TimeTableElement.swift
@@ -0,0 +1,25 @@
+//
+//  TimeTableElement.swift
+//  kadai_kanri
+//
+//  Created by 當山寛人 on 2022/06/02.
+//
+
+import Foundation
+import RealmSwift
+
+class TimeTableElement: Object, ObjectKeyIdentifiable{
+    @Persisted(primaryKey: true) var _id: ObjectId //ID
+    @Persisted var dayOfWeek: String //曜日
+    @Persisted var period: Int //時間
+    @Persisted var className: String //科目名
+    @Persisted var Assignments: List<Assignment>//課題のリスト
+    
+    
+    convenience init(dayOfWeek: String, period: Int,className: String ){
+        self.init()
+        self.dayOfWeek = dayOfWeek
+        self.period = period
+        self.className = className
+    }
+}
